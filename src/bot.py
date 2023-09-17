c548623ccf106efc5e9c80d077e9bb7519d8d824
Netrunner <85088259+IUseDebianBtw@users.noreply.github.com>
2023-09-17 00:15:55 -0500

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -2,7 +2,8 @@ import os
 import discord
 from datetime import datetime
 
-TOKEN = 'your_bot_token_here'
+TOKEN = 'your_bot_token'
+WHITELISTED_SERVER_IDS = [12345679, 123465976]
 
 client = discord.Client()
 
@@ -14,16 +15,17 @@ async def on_ready():
 async def on_message(message):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
-    if message.guild and message.guild in client.guilds:
-        server_folder = f'/path/to/your/dir/{message.guild.id}'
-        os.makedirs(server_folder, exist_ok=True)
-        channel_file = f'{server_folder}/{message.channel.name}.txt'
-        
-        with open(channel_file, 'a') as log_file:
-            log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
-
-    elif message.guild is None and message.author != client.user:
-        dm_folder = f'/path/to/your/dir/DM_{message.author.id}'
+    if message.guild:
+        if message.guild.id in WHITELISTED_SERVER_IDS:  # Check if the guild is whitelisted
+            server_folder = f'/path/to/dir/{message.guild.id}'
+            os.makedirs(server_folder, exist_ok=True)
+            channel_file = f'{server_folder}/{message.channel.name}.txt'
+            
+            with open(channel_file, 'a') as log_file:
+                log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
+                
+    elif message.guild is None and message.author != client.user:  # Log all DMs
+        dm_folder = f'/path/to/dir/DM_{message.author.id}'
         os.makedirs(dm_folder, exist_ok=True)
         dm_file = f'{dm_folder}/DM.txt'
         
