8c0bdc65856ecb533741e4e8aec4c7309be5f78c
Netrunner <85088259+IUseDebianBtw@users.noreply.github.com>
2023-09-17 16:50:03 -0500

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -16,18 +16,18 @@ async def on_message(message):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
     if message.guild:
-        if message.guild.id in WHITELISTED_SERVER_IDS:  # Check if the guild is whitelisted
+        if message.guild.id in WHITELISTED_SERVER_IDS:
             server_folder = f'/path/to/dir/{message.guild.id}'
             os.makedirs(server_folder, exist_ok=True)
-            channel_file = f'{server_folder}/{message.channel.name}.txt'
+            channel_file = f'{server_folder}/{message.channel.name}.md'
             
             with open(channel_file, 'a') as log_file:
                 log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
                 
-    elif message.guild is None and message.author != client.user:  # Log all DMs
+    elif message.guild is None and message.author != client.user:
         dm_folder = f'/path/to/dir/DM_{message.author.id}'
         os.makedirs(dm_folder, exist_ok=True)
-        dm_file = f'{dm_folder}/DM.txt'
+        dm_file = f'{dm_folder}/DM.md'
         
         with open(dm_file, 'a') as log_file:
             log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
