a8b403202dfd70f305b1cce0fea5b173d44b7173
Netrunner <85088259+IUseDebianBtw@users.noreply.github.com>
2023-09-16 23:06:07 -0500

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -13,9 +13,9 @@ async def on_ready():
 @client.event
 async def on_message(message):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
+    
     if message.guild and message.guild in client.guilds:
-        server_folder = f'server_{message.guild.id}'
+        server_folder = f'/path/to/your/dir/{message.guild.id}'
         os.makedirs(server_folder, exist_ok=True)
         channel_file = f'{server_folder}/{message.channel.name}.txt'
         
@@ -23,7 +23,7 @@ async def on_message(message):
             log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
 
     elif message.guild is None and message.author != client.user:
-        dm_folder = f'DM_{message.author.id}'
+        dm_folder = f'/path/to/your/dir/DM_{message.author.id}'
         os.makedirs(dm_folder, exist_ok=True)
         dm_file = f'{dm_folder}/DM.txt'
         
@@ -31,5 +31,3 @@ async def on_message(message):
             log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
 
 client.run(TOKEN)
-
-
