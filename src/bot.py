8bd9ccf58ca3e126d19127745ff22362e75ea15d
LyubomirT <terlyubomir@gmail.com>
2023-09-19 12:56:22 +0300

    Constants for directories

--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,11 @@ from datetime import datetime
 TOKEN = 'your_bot_token'
 WHITELISTED_SERVER_IDS = [12345679, 123465976]
 
+# Define constants for log directory paths
+LOG_DIR = '/path/to/dir/'
+SERVER_LOG_DIR = os.path.join(LOG_DIR, 'server_logs')
+DM_LOG_DIR = os.path.join(LOG_DIR, 'dm_logs')
+
 client = discord.Client()
 
 @client.event
@@ -17,10 +22,10 @@ async def on_message(message):
     
     if message.guild:
         if WHITELISTED_SERVER_IDS == 'all' or message.guild.id in WHITELISTED_SERVER_IDS:
-            server_folder = f'/path/to/dir/{message.guild.id}'
+            server_folder = os.path.join(SERVER_LOG_DIR, str(message.guild.id))
             try:
                 os.makedirs(server_folder, exist_ok=True)
-                channel_file = f'{server_folder}/{message.channel.name}.md'
+                channel_file = os.path.join(server_folder, f'{message.channel.name}.md')
                 
                 with open(channel_file, 'a') as log_file:
                     log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
@@ -28,10 +33,10 @@ async def on_message(message):
                 print(f'Error writing to {channel_file}: {e}')
                 
     elif message.guild is None and message.author != client.user:
-        dm_folder = f'/path/to/dir/DM_{message.author.id}'
+        dm_folder = os.path.join(DM_LOG_DIR, f'DM_{message.author.id}')
         try:
             os.makedirs(dm_folder, exist_ok=True)
-            dm_file = f'{dm_folder}/DM.md'
+            dm_file = os.path.join(dm_folder, 'DM.md')
             
             with open(dm_file, 'a') as log_file:
                 log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
