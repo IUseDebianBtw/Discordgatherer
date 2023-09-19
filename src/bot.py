a7c8da35d8feef8aca196288700a3102081a3694
LyubomirT <terlyubomir@gmail.com>
2023-09-19 12:53:58 +0300

    Error Handling

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,6 @@ import discord
 from datetime import datetime
 
 TOKEN = 'your_bot_token'
-# change the whitelisted server ids to all to log all servers, if not just add the server id
 WHITELISTED_SERVER_IDS = [12345679, 123465976]
 
 client = discord.Client()
@@ -19,18 +18,24 @@ async def on_message(message):
     if message.guild:
         if WHITELISTED_SERVER_IDS == 'all' or message.guild.id in WHITELISTED_SERVER_IDS:
             server_folder = f'/path/to/dir/{message.guild.id}'
-            os.makedirs(server_folder, exist_ok=True)
-            channel_file = f'{server_folder}/{message.channel.name}.md'
-            
-            with open(channel_file, 'a') as log_file:
-                log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
+            try:
+                os.makedirs(server_folder, exist_ok=True)
+                channel_file = f'{server_folder}/{message.channel.name}.md'
+                
+                with open(channel_file, 'a') as log_file:
+                    log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
+            except Exception as e:
+                print(f'Error writing to {channel_file}: {e}')
                 
     elif message.guild is None and message.author != client.user:
         dm_folder = f'/path/to/dir/DM_{message.author.id}'
-        os.makedirs(dm_folder, exist_ok=True)
-        dm_file = f'{dm_folder}/DM.md'
-        
-        with open(dm_file, 'a') as log_file:
-            log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
+        try:
+            os.makedirs(dm_folder, exist_ok=True)
+            dm_file = f'{dm_folder}/DM.md'
+            
+            with open(dm_file, 'a') as log_file:
+                log_file.write(f'{timestamp} | {message.author.display_name}: {message.content}\n')
+        except Exception as e:
+            print(f'Error writing to {dm_file}: {e}')
 
 client.run(TOKEN)
